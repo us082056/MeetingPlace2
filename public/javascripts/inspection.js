0acb811d9f81685ddafc60a2516469576d1e692e
masayuki <>
2018-07-11 21:21:33 +0900

    implement inspection page and logic

--- public/javascripts/inspection.js
+++ public/javascripts/inspection.js
@@ -1,16 +1,30 @@
 const mp = {
     inspectionControl : {
         init: function() {
-            var _self = this;
+            var _self = this,
+                $msg = $("<p class='mp-errormsg alert alert-info'/>");
 
-            $("#mp-deletebutton").on("click", function() {
-                if ($(".mp-stationform").length === _self.MINIMUM_FORM_COUNT) {
-                    return;
-                }
+            $msg.append("下記の駅は複数の候補があります、検索に使用する駅を選択してください。");
+            $("#mp-messagearea").append($msg);
 
-                $(".mp-stationform:last").remove();
+            $("#mp-backbutton").on("click", function() {
+                location.href = "index";
+            });
 
-                _self._updateDeleteButtonState();
+            $("#mp-searchbutton").on("click", function() {
+                var url = "search?",
+                    $input = $(".mp-stationform").find("input, select");
+
+                $input.each(function(idx, elm) {
+                    url += "station" + (idx + 1) + "=" + $(elm).val();
+
+                    if (idx !== ($input.length - 1)) {
+                        url += "&";
+                    }
+                });
+
+                // navigate next page
+                location.href = url;
             });
         }
     }
