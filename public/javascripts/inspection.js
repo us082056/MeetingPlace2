428cac2f4760fa2ca3ea233dad6a29dc8cda513b
masayuki <>
2018-07-11 21:38:47 +0900

    refactoring

--- public/javascripts/inspection.js
+++ public/javascripts/inspection.js
@@ -1,8 +1,7 @@
 const mp = {
     inspectionControl : {
         init: function() {
-            var _self = this,
-                $msg = $("<p class='mp-errormsg alert alert-info'/>");
+            var $msg = $("<p class='mp-errormsg alert alert-info'/>");
 
             $msg.append("下記の駅は複数の候補があります、検索に使用する駅を選択してください。");
             $("#mp-messagearea").append($msg);
